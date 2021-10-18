485c489420aaa19db011c75a147cceb07a88c2d8
Floffah <27270386+Floffah@users.noreply.github.com>
2021-10-18 10:00:42 +0100

    feat(Context): add creatable defaults

--- src/context/defaults/index.ts
+++ src/context/defaults/index.ts
@@ -3,6 +3,7 @@ import { ContextValues } from "../types";
 import { strings } from "./types/strings";
 import { arrays } from "./types/arrays";
 import { io } from "./io";
+import { Context } from "../Context";
 
 export const allDefaults: ContextValues = {
     ...variables,
@@ -10,3 +11,11 @@ export const allDefaults: ContextValues = {
     ...arrays,
     ...io,
 };
+
+export const defaults = { variables, strings, arrays, io };
+
+export const createFullContext = () => Context.extend(allDefaults);
+export const createIsolatedContext = () =>
+    Context.extend({ ...variables, ...strings, ...arrays });
+export const createTypesOnlyContext = () =>
+    Context.extend({ ...strings, ...arrays });
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import grammar, { lexerRules } from "./lang/lang";
 
 export { grammar, lexerRules };
 export * from "./ast/index";
+export * from "./context/defaults/index";
 export * from "./context/Context";
 export * from "./context/types";
 export * from "./context/util";
@@ -9,4 +10,3 @@ export * from "./runtime/Process";
 export * from "./runtime/Run";
 export * from "./runtime/Runtime";
 export * from "./util/ScriptError";
-export * from "./context/defaults/index";
